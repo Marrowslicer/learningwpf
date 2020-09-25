ef9a5d604c1d89c40a0f5a48909b3d2bdc4f9224
Marrowslicer <marrowslicer@gmail.com>
2020-09-25 22:02:42 +0300

    Add StringExtensions class

---
+++ LearningWPF/P008_LearningFileSystem/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace P008_LearningFileSystem
+{
+    public static class StringExtensions
+    {
+        public static string Args(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
