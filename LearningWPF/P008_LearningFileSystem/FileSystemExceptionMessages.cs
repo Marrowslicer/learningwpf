eca5756dd338efbbfbbfbaa764fd200e44b16588
Marrowslicer <marrowslicer@gmail.com>
2020-09-25 22:23:38 +0300

    Change DIRECTORY_NOT_FOUND_ERROR message

--- LearningWPF/P008_LearningFileSystem/FileSystemExceptionMessages.cs
+++ LearningWPF/P008_LearningFileSystem/FileSystemExceptionMessages.cs
@@ -7,7 +7,7 @@
         public const string CREATE_FILE_ERROR = "Create file error: path - {0}";
         public const string DELETE_DIRECTORY_ERROR = "Delete directory error: path - {0}";
         public const string DELETE_FILE_ERROR = "Delete file error: path - {0}";
-        public const string DIRECTORY_NOT_FOUND_ERROR = "Source directory does not exist or could not be found: path - {0}";
+        public const string DIRECTORY_NOT_FOUND_ERROR = "Directory does not exist or could not be found: path - {0}";
         public const string MOVE_DIRECTORY_ERROR = "Move directory error: source - {0}; destination - {1}";
         public const string MOVE_FILE_ERROR = "Move file error: source - {0}; destination - {1}";
     }
