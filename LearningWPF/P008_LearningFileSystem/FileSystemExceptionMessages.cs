3728679ed57b5d9ee45f2899c87e6a4cf189478c
Marrowslicer <marrowslicer@gmail.com>
2020-09-25 22:03:37 +0300

    Add FileSystemExceptionMessages class

---
+++ LearningWPF/P008_LearningFileSystem/FileSystemExceptionMessages.cs
@@ -0,0 +1,14 @@
+﻿namespace P008_LearningFileSystem
+{
+    public static class FileSystemExceptionMessages
+    {
+        public const string COPY_ERROR = "Copy error: source - {0}; destination - {1}";
+        public const string CREATE_DIRECTORY_ERROR = "Create directory error: path - {0}";
+        public const string CREATE_FILE_ERROR = "Create file error: path - {0}";
+        public const string DELETE_DIRECTORY_ERROR = "Delete directory error: path - {0}";
+        public const string DELETE_FILE_ERROR = "Delete file error: path - {0}";
+        public const string DIRECTORY_NOT_FOUND_ERROR = "Source directory does not exist or could not be found: path - {0}";
+        public const string MOVE_DIRECTORY_ERROR = "Move directory error: source - {0}; destination - {1}";
+        public const string MOVE_FILE_ERROR = "Move file error: source - {0}; destination - {1}";
+    }
+}
