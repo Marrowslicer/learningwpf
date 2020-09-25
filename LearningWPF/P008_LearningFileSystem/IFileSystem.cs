4641b57a1fc28c1996147cd83a94a7a70e08511c
Marrowslicer <marrowslicer@gmail.com>
2020-09-25 22:04:18 +0300

    Add IFileSystem interface

---
+++ LearningWPF/P008_LearningFileSystem/IFileSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace P008_LearningFileSystem
+{
+    public interface IFileSystem
+    {
+        void CopyDirectory(string sourceDirectoryName, string destinationDirectoryName, bool recursive);
+        void CopyFile(string sourceFileName, string destinationFileName, bool overwrite);
+        DirectoryInfo CreateDirectory(string path);
+        FileStream CreateFile(string path);
+        void DeleteDirectory(string path, bool recursive);
+        void DeleteFile(string path);
+        bool DirectoryExist(string path);
+        bool FileExist(string path);
+        void MergeDirectories(string sourcePath, string destinationPath);
+        void MoveDirectory(string sourceDirectoryName, string destinationDirectoryName);
+        void MoveFile(string sourceFileName, string destinationFileName);
+    }
+}
