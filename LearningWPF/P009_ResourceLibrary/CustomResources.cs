14a78ae864192cba359774a04bef69dc15e9a4ff
Marrowslicer <marrowslicer@gmail.com>
2020-09-27 20:29:15 +0300

    Create a new ResourceLibrary project

---
+++ LearningWPF/P009_ResourceLibrary/CustomResources.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace P009_ResourceLibrary
+{
+    public class CustomResources
+    {
+        public static ComponentResourceKey RedSolidBrush
+        {
+            get
+            {
+                return new ComponentResourceKey(typeof(CustomResources), "RedSolidBrush");
+            }
+        }
+
+        public static ComponentResourceKey GreenSolidBrush
+        {
+            get
+            {
+                return new ComponentResourceKey(typeof(CustomResources), "GreenSolidBrush");
+            }
+        }
+    }
+}
